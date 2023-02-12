e42b5f13d5d15824725eb5764c24a06711db5d0b
Sergei Vasiliev <9670105747@mail.ru>
2023-02-12 19:41:10 +0700

    Теория и практика по массивам и алгоритмам

---
+++ C#_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+//Console.WriteLine("Hello World!"); // Вывод строки 
+ // "Hello World!" в консольное окно 
+// TODO: Implement Functionality Here — здесь должны быть 
+// операторы приложения, задающие функциональность самого 
+// приложения, т. е. что приложение должно делать 
+Console.Write("Press any key to continue..."); 
+/* Выводится в консольное окно сообщение "Press any key to 
+ continue... " (нажмите любую клавишу для продолжения) */ 
+Console.ReadKey(true); // Задержка экрана
+
